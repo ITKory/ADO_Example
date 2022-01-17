19c8cc810f7a64426cfdb5528100172f81e3ea51
Starinin Andrey <starininandrey@gmail.com>
2022-01-17 21:15:43 +0300

    Update Lib

--- ADO_Example.Lib/StudentsDb.cs
+++ ADO_Example.Lib/StudentsDb.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using ADO_Example.Model;
 using MySql.Data.MySqlClient;
 
 namespace ADO_Example.App
@@ -25,7 +27,43 @@ namespace ADO_Example.App
         public void Open() => _db.Open();
 
         public void Close() => _db?.Close();
-        
-        
+
+        public bool AddPerson(Person person)
+        {
+            Open();
+            
+            var sql = $"INSERT INTO tab_persons (first_name, last_name) VALUES ('{person.FirstName}', '{person.LastName}');";
+            _command.CommandText = sql;
+            var res = _command.ExecuteNonQuery();
+            
+            Close();
+            
+            return res == 1;
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            var persons = new List<Person>();
+            
+            Open();
+
+            var sql = "SELECT id, first_name, last_name FROM tab_persons";
+            _command.CommandText = sql;
+
+            var res = _command.ExecuteReader();
+            while (res.Read())
+            {
+                persons.Add(new Person
+                {
+                    Id = res.GetInt32("id"),
+                    FirstName = res.GetString("first_name"),
+                    LastName = res.GetString("last_name"),
+                });
+            }
+            
+            Close();
+
+            return persons;
+        }
     }
 }
