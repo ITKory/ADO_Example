8ccf7533eab5e6d5e0b4f8ff6acb56158c3d4569
Starinin Andrey <starininandrey@gmail.com>
2022-01-17 20:59:01 +0300

    Init Lib

---
+++ ADO_Example.Lib/StudentsDb.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace ADO_Example.App
+{
+    public class StudentsDb
+    {
+        private readonly MySqlConnection _db;
+        private MySqlCommand _command;
+
+        public StudentsDb()
+        {
+            var str = File.ReadAllText("connect_to_db.ini"); //TODO Сделать проверку файла
+            _db = new MySqlConnection(str);
+            _command = new MySqlCommand { Connection = _db};
+        }
+
+        public StudentsDb(string connectionString)
+        {
+            //TODO Сделать проверку входящего аргумента
+            _db = new MySqlConnection(connectionString);
+            _command = new MySqlCommand { Connection = _db};
+        }
+
+        public void Open() => _db.Open();
+
+        public void Close() => _db?.Close();
+        
+        
+    }
+}
