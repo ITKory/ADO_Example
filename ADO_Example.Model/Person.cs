df0d345be64e0879d42e966ce1bab43a80a6b67b
Starinin Andrey <starininandrey@gmail.com>
2022-01-17 21:15:31 +0300

    Add Model

---
+++ ADO_Example.Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ADO_Example.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
